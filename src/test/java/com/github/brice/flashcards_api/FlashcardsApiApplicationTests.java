3bfbbe12d497a408513b32754a0960a47b71283b
B-Mikembo <mikembo.brice.pro@gmail.com>
2024-11-15 21:51:09 +0100

    init commit

---
+++ src/test/java/com/github/brice/flashcards_api/FlashcardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brice.flashcards_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
